5e4c785a9d4cc91450982c4215f22a312d530e29
jcose <jcose13@gmail.com>
2020-04-23 17:13:26 -0600

    Makefile modification to compile and run pruebaShM

--- semana2/pruebaShM.cc
+++ semana2/pruebaShM.cc
@@ -2,35 +2,45 @@
 #include <sys/types.h>
 #include <sys/shm.h>
 #include <string.h>
-#include <wait.h>
+//#include <wait.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
 
 #include "Semaphore.h"
 
 int main() {
 
-   int i;
-   Semaforo s;
-   int id = shmget( 123456, 1024, 0700 | IPC_CREAT );
-   char * area = (char *) shmat( id, NULL, 0 );
-   char * var = "Area de memoria compartida 2020";
-   char newvar[100];
+    int i;
+    Semaphore s;
+    int id = shmget( 32135, 1024, 0700 | IPC_CREAT );
+    char * area = (char *) shmat( id, NULL, 0 );
+    char * var = (char *)("Area de memoria compartida 2020");
+    char newvar[100];
+    pid_t pid;
 
-   if ( fork() ) {
-      strcpy( area, var );
-      printf( "Escriba un numero y <ENTER> para continuar ...\n" );
-      scanf( "%i", &i );
-      printf( "Variable es %s \n", area );
-      s.Signal();
-   }
-   else {
-      s.Wait();
-      strcpy( newvar, area );
-      printf( "La variable compartida es: %s \n", newvar );
-      exit( 0 );
-   }
-   wait( &i );
-   printf( "Fin del programa ... \n" );
-   shmdt( area );
-   shmctl( id, IPC_RMID, NULL );
+    if ( (pid = fork()) > 1 ) {
+        strcpy( area, var );
+        printf( "Escriba un numero y <ENTER> para continuar ...\n" );
+        scanf( "%i", &i );
+        printf( "Variable es %s \n", area );
+        s.Signal();
+    }
+    else if(pid == 0){
+        s.Wait();
+        strcpy( newvar, area );
+        printf( "La variable compartida es: %s\n", newvar);
+        exit( 0 );  //closes child proccess
+    }
+    else
+    {
+        perror("fork");
+                exit(1);
+    }
+    wait(&i);
+    printf( "Fin del programa ...\n");
+    shmdt( area );
+    shmctl( id, IPC_RMID, NULL );
 }
 
