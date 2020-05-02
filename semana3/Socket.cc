a4485cddafa833b413f5a9c49bc4b4f95ac2cf5f
Jefferson Cose <jcose13@gmail.com>
2020-05-01 20:01:21 -0600

    connect method changes, gethostbyname and getprotobyname addition

--- semana3/Socket.cc
+++ semana3/Socket.cc
@@ -1,6 +1,6 @@
 #include <errno.h>
-//#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -16,30 +16,24 @@
    bool ipv6: si queremos un socket para IPv6
  */
 Socket::Socket( char tipo, bool ipv6 ): type( tipo ), ipv6( ipv6 ) {
-    //int socket(int domain, int type, int protocol);
-    //domain AF_INET or AF_UNIX
-    //type SOCK_STREAM or SOCK_DGRAM
-    //protocol 0 para que la eleccion la haga el sistema
-    //return director de fichero o -1 en caso de error. errno
-    
     idSocket = 0;
-    if( type == 's' ){
-    	if(ipv6){
+    if( type == 's' ){	// 's' para "stream"
+    	if(ipv6){	// ipv6: si queremos un socket para IPv6
     	    	idSocket = socket( AF_INET6, SOCK_STREAM, 0 );
     	}
     	else {
     		idSocket = socket( AF_INET, SOCK_STREAM, 0 );
     	}
     }
-    else if( type == 'd' ){
-    	if(ipv6){
+    else if( type == 'd' ){	// 'd' para "datagram"
+    	if(ipv6){	// ipv6: si queremos un socket para IPv6
     	    	idSocket = socket( AF_INET6, SOCK_DGRAM, 0 );
     	}
     	else {
     		idSocket = socket( AF_INET, SOCK_DGRAM, 0 );
     	}
     }
-    else{
+    else{	
 	printf("Socket constructor failed. Type parameter must be 's' or 'd', but '%c' was provided\n", type);
     }
     
@@ -48,11 +42,9 @@ Socket::Socket( char tipo, bool ipv6 ): type( tipo ), ipv6( ipv6 ) {
     }
 }
 
-
 Socket::~Socket(){
-    Close();
+    	Close();
 }
-
 
 void Socket::Close(){
 	if( close(idSocket) == -1 )
@@ -64,84 +56,112 @@ void Socket::Close(){
    int port: ubicacion del proceso, por ejemplo 80
  */
 int Socket::Connect( char * hostip, int port ) {
-	struct sockaddr_in address;
-	if( type == 's' ) {
-		address.sin_addr.s_addr = inet_addr( hostip );
+	int status;
+	char * host = hostip;
+	struct hostent *hostbyname;
+	
+	hostbyname = gethostbyname( hostip );
+	if( hostbyname != nullptr ) {
+		host = hostbyname->h_name;
+	}
+
+	if( ipv6 ) {
+		struct sockaddr_in6 s_address;
+		s_address.sin6_family = AF_INET6;
+		inet_pton( AF_INET6, host, &s_address.sin6_addr );	
+		s_address.sin6_port = htons( port );
+		status = connect( idSocket, ( struct sockaddr* ) &s_address, sizeof( s_address ) );
+	}
+	else {
+		struct sockaddr_in address;
+		address.sin_addr.s_addr = inet_addr( host );
 		address.sin_family = AF_INET;
 		address.sin_port = htons( port );
-		if( connect( idSocket, (struct sockaddr*) &address, sizeof(address) ) == -1 ) {
-			printf("Connect failed: %d\n", errno);
-			return -1;
-		}
+		status = connect( idSocket, ( struct sockaddr* ) &address, sizeof( address ) );
 	}
-   	return 0;
-}
 
+	if(status == -1) {
+		printf("Connect failed: %d\n", errno);
+	}
+   	return status;
+   	
+}
 
 /*
    char * hostip: direccion del servidor, por ejemplo "www.ecci.ucr.ac.cr"
    char * service: nombre del servicio que queremos acceder, por ejemplo "http"
  */
 int Socket::Connect( char *host, char *service ) {
-	struct sockaddr_in6 s_address;
-	if( type == 's' ) {
+   	int status, port;
+	struct protoent *proto;
+	proto = getprotobyname(service);
+	if(proto != nullptr) {
+		port = proto->p_proto;
+	}
+	else if ( strcmp( service, "http" ) == 0 ){
+		port = 7002;
+	}
+	else {
+		port = 12321;
+	}
+   	
+	if( ipv6 ) {
+		struct sockaddr_in6 s_address;
 		s_address.sin6_family = AF_INET6;
-		inet_pton(AF_INET6, /*host*/"::1", &s_address.sin6_addr);
-		//convertir service a int
-		
-		s_address.sin6_port = htons(80);
-		if( connect( idSocket, (struct sockaddr*) &s_address, sizeof(s_address) ) == -1 ) {
-			printf("Connect failed: %d\n", errno);
-			return -1;
-		}
-	}	
-   return 0;
+		inet_pton( AF_INET6, host, &s_address.sin6_addr );	
+		s_address.sin6_port = htons( port );
+		status = connect( idSocket, ( struct sockaddr* ) &s_address, sizeof( s_address ) );
+	}
+	else {
+		struct sockaddr_in address;
+		address.sin_addr.s_addr = inet_addr( host );
+		address.sin_family = AF_INET;
+		address.sin_port = htons( port );
+		status = connect( idSocket, ( struct sockaddr* ) &address, sizeof( address ) );
+	}
+
+	if(status == -1) {
+		printf("Connect failed: %d\n", errno);
+	}
+   	return status;
 }
 
-
 int Socket::Read( char *text, int len ) {
-	int value;
-	if( ( value = read( idSocket, ( void * )text, len ) ) == -1 ) {
+	int value = read( idSocket, ( void * )text, len );
+	if( value == -1 ) {
 		printf( "Socket Read: %d\n", errno );
 	}
    	return value;
 }
 
-
 int Socket::Write( char *text , int len) {
-	int value;
-	if( (value = write( idSocket, ( const void *) text, len ) ) == -1 ) {
+	int value = write( idSocket, ( const void *) text, len );
+	if( value == -1 ) {
 		printf( "Socket Write: %d\n", errno );
 	}
    	return value;
 }
 
 int Socket::Write( char *text ) {
-	int value;
-	if( (value = write( idSocket, ( const void *) text, strlen(text) ) ) == -1 ) {
+	int value = write( idSocket, ( const void *) text, strlen(text) );
+	if( value == -1 ) {
 		printf( "Socket Write: %d\n", errno );
 	}
    	return value;
 }
 
-
 int Socket::Listen( int queue ) {
-
-    return -1;
-
+	return -1;
 }
 
-
 int Socket::Bind( int port ) {
-	struct sockaddr_un su;
+	/*struct sockaddr_un su;
    	if(idSocket == -1){
     		su.sun_family = AF_INET;
     		strcpy( su.sun_path, "/semana3/mysocketinet");
-    	}
+    	}*/
 	return -1;
-
 }
-
 
 //Socket * Socket::Accept(){
 
@@ -149,17 +169,11 @@ int Socket::Bind( int port ) {
 
 //}
 
-
 int Socket::Shutdown( int mode ) {
-
-    return -1;
-
+    	return -1;
 }
 
-
 void Socket::SetIDSocket(int id){
-
-    idSocket = id;
-
+    	idSocket = id;
 }
 
--- semana3/ipv6-test.cc
+++ semana3/ipv6-test.cc
@@ -8,8 +8,10 @@ int main( int argc, char * argv[] ) {
    char a[512];
 
    memset( a, 0, 512 );
-   s.Connect( (char *) "fe80::ffb4:ea86:1aa4:19c%eno1", (char *) "http" );
-   s.Write(  (char *) "GET / HTTP/1.1\r\nhost: redes.ecci\r\n\r\n", 36 );
+   //"fe80::ffb4:ea86:1aa4:19c%eno1" prof
+   //en0 "fe80::10a1:c47f:9de2:291f%en0"
+   s.Connect( (char *) "::1", (char *) "http" );
+   s.Write( ( char* )"GET / HTTP/1.1\r\nhost: redes.ecci\r\n\r\n", 36 );
    s.Read( a, 512 );
    printf( "%s\n", a);
 }
